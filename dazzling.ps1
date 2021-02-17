ce03a1d484a99de4f912be46c385624b05c3c586
Razzle F. Dazzle <rfdazzle@pm.me>
2021-02-16 22:23:12 -0500

    updated ps1

--- dazzling.ps1
+++ dazzling.ps1
@@ -4,6 +4,7 @@
 $DCSPath=Get-ChildItem HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\* | % { Get-ItemProperty $_.PsPath } | Select-Object InstallLocation | Where-Object InstallLocation -Like "*DCS*" | ForEach-Object {$_.InstallLocation}
 $TargetPath=Get-ChildItem HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | % { Get-ItemProperty $_.PsPath } | Select-Object InstallLocation | Where-Object InstallLocation -Like "*Thrustmaster*" | ForEach-Object {$_.InstallLocation}
 $F16CInput=$DCSPath+"Mods\aircraft\F-16C\Input\F-16C\joystick\"
+$DCSPath+"Mods\aircraft\A-10C_2\Input\A-10C_2\joystick"
 $UserProfile=[System.Environment]::GetEnvironmentVariable('userprofile')
 $BackupDirectory=$null
 $BackupDirectory=Test-Path -Path "$UserProfile\SkunkworksBackup\"
@@ -44,7 +45,7 @@ function Show-Menu {
     Clear-Host
     Write-Host "================ $Title ================"
     Write-Host ""
-    Write-Host "Choose your fate..."
+    Write-Host "Choose your fate...TEST YOUR MIGHT!"
     Write-Host ""
     Write-Host "1: Backup stock default.lua"
     Write-Host "2: Overwrite stock F16-C default.lua"
@@ -60,10 +61,11 @@ do
     $selection = Read-Host "Please make a selection"
     switch ($selection)
     {
-    '1' {'Backing up stock default.lua files';Copy-Item "$F16CInput\default.lua" -Destination $UserProfile\SkunkworksBackup\}
-    '2' {"F16C_default.lua has been copied to $F16CInput";Copy-Item ".\F16C_default.lua" -Destination "C:\DCS Mods\temp\"}
+    '1' {'Backing up stock default.lua files';Copy-Item "$F16CInput\default.lua" -Destination $UserProfile\SkunkworksBackup\F16C_default.lua}
+    '2' {"Copying F16C_default.lua to $F16CInput, overwriting default.lau";Copy-Item ".\F16C\F16C_default.lua" -Destination "$F16CInput\default.lua"}
+    '3' {{'Backing up stock default.lua files';Copy-Item "$F16CInput\default.lua" -Destination $UserProfile\SkunkworksBackup\F16C_default.lua}
         }
-    
+    }
     pause
     
  }
